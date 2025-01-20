f163c85db25115a21f1498708c9df017e8d7a12b
gaellerem <146330927+gaellerem@users.noreply.github.com>
2025-01-20 11:44:42 +0100

    git init

---
+++ src/test/java/com/aggroConnect/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aggroConnect.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
